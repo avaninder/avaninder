7bd6eae89c0892c7f6b071f351f9e28202b8e8ef
Avaninder Bhaghayath <charizardavi@gmail.com>
2023-04-03 23:30:00 -0400

    remove mobile stuff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       </div>
 
       {/* <ComputersCanvas /> */}
-      <video style="pointer-events: none;" src="./output/drone.mp4" autoPlay muted className="w-full"></video>
+      <video src="./output/drone.mp4" autoPlay muted className="w-full"></video>
       {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
