e386fd374f24e73ca6a16146553dc641db01e182
Avaninder Bhaghayath <charizardavi@gmail.com>
2026-08-29 01:05:16 -0400

    eslint fix

--- app/page.tsx
+++ app/page.tsx
@@ -235,7 +235,7 @@ export default function Home() {
             height={384}
           />
           <p>
-            Hey! I'm Avaninder, and I'm currently studying EE & CS at Yale. My background is in competitive robotics, and I'm continuing to put those skills to use across industries. If you want to reach out, my contact info is on LinkedIn (below).
+            Hey! I&apos;m Avaninder, and I&apos;m currently studying EE & CS at Yale. My background is in competitive robotics, and I&apos;m continuing to put those skills to use across industries. If you want to reach out, my contact info is on LinkedIn (below).
           </p>
         </section>
 
